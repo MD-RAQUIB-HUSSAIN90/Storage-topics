3472db9c8c4379c3458459194fc9ddf9cfec9e24
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-06-17 00:53:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// localStorage
+//  what have to learn in it{
+// i   how to store data
+// ii  how to fetch deta
+// iii how to remove data
+// iv  how to update date
+//  }
+
+// storing a data
+// localStorage.setItem("name","Arsh khan")
+
+// fetching a data
+// let print=localStorage.getItem("name")
+// console.log(print);
+
+// removing a data
+// localStorage.removeItem("name");
+
+// Updating a data
+// localStorage.setItem("name","raquib")
+
+
+
+
+// sessionStorage
+//  what have to learn in it{
+// i   how to store data
+// ii  how to fetch deta
+// iii how to remove data
+// iv  how to update date
+//  }
+
+// sessionStorage.setItem('name',"arsh");
+// sessionStorage.setItem('name',"raquib")
+// let print=sessionStorage.getItem("name")
+// console.log(print);
+// sessionStorage.removeItem("name")
+// sessionStorage.clear("name")
+
+
+// cookies Storage
